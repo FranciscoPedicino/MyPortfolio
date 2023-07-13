685e7cf6cdda34140c9e552d7c8fe472a47d09db
FranciscoPedicino <franciscopedi91@gmail.com>
2023-07-13 15:14:05 -0300

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className="h-20">
-      <h2>NavBar</h2>
+    <div className="h-40 flex justify-center items-center ">
+      <div className='w-[40%] flex bg-black justify-around '>
+      <button className='w-20 bg-green-600 h-10 border-r-transparent'>Home</button>
+      <button className='w-20 bg-green-600 h-10 border-r-transparent'>Sobre Mi </button>
+      <button className='w-20 bg-green-600 h-10 border-r-transparent'>Skills</button>
+      <button className='w-20 bg-green-600 h-10 border-r-transparent'>Portfolio</button>
+      <button className='w-20 bg-green-600 h-10 border-r-transparent'>Contacto</button>
+      </div>
     </div>
   )
 }
