6b25839d602ba363bd106c802eb7fa24b3b8c39c
FranciscoPedicino <franciscopedi91@gmail.com>
2023-07-14 01:17:46 -0300

    fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,10 @@ export default function NavBar() {
     <div className="h-32 flex justify-center items-center bg-blue-700 ">
       <div className='w-[40%] h-16 flex bg-black justify-around items-center'>
       <button className='w-24 bg-green-600 h-10 rounded-full '>Home</button>
-      <button className='w-24 bg-green-600 h-10 rounded-full '>Sobre Mi </button>
+      <button className='w-24 bg-green-600 h-10 rounded-full '>About Me</button>
       <button className='w-24 bg-green-600 h-10 rounded-full '>Skills</button>
       <button className='w-24 bg-green-600 h-10 rounded-full '>Portfolio</button>
-      <button className='w-24 bg-green-600 h-10 rounded-full '>Contacto</button>
+      <button className='w-24 bg-green-600 h-10 rounded-full '>Contact</button>
       </div>
     </div>
   )
