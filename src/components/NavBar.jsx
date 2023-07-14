1b5f6aea358e26d2174cf7fa758318aa0724d197
FranciscoPedicino <franciscopedi91@gmail.com>
2023-07-14 19:29:29 -0300

    arreglo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import {Link as Anchor } from "react-router-dom"
 
 export default function NavBar() {
   return (
-    <div className="h-32 flex justify-center items-center bg-blue-700 ">
-      <div className='w-[40%] h-16 flex bg-black justify-around items-center'>
-       <Anchor to="/"><button className='w-24 bg-green-600 h-10 rounded-full '>Home</button></Anchor>
-       <Anchor to='/AboutMe'> <button className='w-24 bg-green-600 h-10 rounded-full '>About Me</button></Anchor> 
-      <button className='w-24 bg-green-600 h-10 rounded-full '>Skills</button>
-      <button className='w-24 bg-green-600 h-10 rounded-full '>Portfolio</button>
-      <button className='w-24 bg-green-600 h-10 rounded-full '>Contact</button>
+    <div className="h-24 flex justify-center items-center bg-[#F5F5F5]  ">
+      <div className='w-[40%] h-16 flex justify-around items-center'>
+       <Anchor to="/"><button className='w-24  h-12 font-semibold rounded-full '>Home</button></Anchor>
+       <Anchor to='/AboutMe'> <button className='w-24  h-12 font-semibold rounded-full hover:bg-[#0ACF83]  '>About Me</button></Anchor> 
+      <button className='w-24  h-12 font-semibold rounded-full hover:bg-[#0ACF83]  '>Skills</button>
+      <button className='w-24  h-12 font-semibold rounded-full hover:bg-[#0ACF83] '>Portfolio</button>
+      <button className='w-24  h-12 font-semibold rounded-full   hover:bg-[#0ACF83] '>Contact</button>
       </div>
     </div>
   )
